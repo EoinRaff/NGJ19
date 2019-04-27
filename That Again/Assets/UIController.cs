ec1b00fbd8accea02b3da40abf3dcf8057e0728a
EoinRaff <eoinrafferty@gmail.com>
2019-04-27 15:13:24 +0200

    added ultra basic UI but need to fix spawn script

---
+++ That Again/Assets/UIController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+    public TextMeshProUGUI yearsText;
+    public Slider temperature;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float years = GameManager.Instance.gameTime / 10f;
+        yearsText.text = "Years: " + Mathf.Floor(years);
+        temperature.value = GameManager.Instance.CurrentTemperature;
+    }
+}
